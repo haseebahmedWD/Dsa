34c48e11d73c4afea134bacd93b46412f620af8e
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-09-24 15:06:15 +0500

    updated file

---
+++ queueCircular.c
@@ -0,0 +1,118 @@
+// This program is about Circular Queue implementation using array
+#include<stdio.h>
+#include<stdlib.h>
+struct Queue
+{
+    int size;
+    int front;  //used for front pointer
+    int rear;   //used for rear pointer
+    int *ptr;   //point the memory allocated on heap
+};
+struct Queue *createQueue(struct Queue *arr, int qSize)
+{
+    arr = (struct Queue *)malloc(sizeof(struct Queue)); //creating queue
+    arr->size = qSize;
+    arr->front = -1;
+    arr->rear = -1;
+    arr->ptr = (int *)malloc(qSize * sizeof(int));  //creating memory on heap
+    printf("*****Queue of size %d is created successfully***** \n",qSize);
+    return arr;
+}
+int isEmpty(struct Queue *arr)
+{
+    if ((arr->front == arr->rear))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+int isFull(struct Queue *arr)
+{
+    if ((arr->front == -1 && arr->rear == arr->size-1))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void firstValue(struct Queue *arr)
+{
+    printf(" %d ",arr->ptr[arr->front]);
+}
+void lastValue(struct Queue *arr)
+{
+    printf(" %d ",arr->ptr[arr->rear]);
+}
+struct Queue *Enqueue(struct Queue *arr)
+{
+    int val = 0;
+    printf("Enter value to insert in queue: ");
+    scanf("%d",&val);
+    arr->rear = (arr->rear + 1) % arr->size;
+    arr->ptr[arr->rear] = val;
+    return arr;
+   
+}
+ int Dequeue(struct Queue *arr)
+{
+    int val;
+    val = arr->ptr[arr->front];
+    arr->ptr[arr->front] = 0;
+    arr->front++;
+    printf(" %d ",val);
+    return val;
+   
+}
+int peak(struct Queue *arr)
+{
+    int i = 0;
+    printf("Enter index to search value(0-%d): ",arr->rear);
+    scanf("%d",&i);
+    if (i >= 0 && i<=arr->rear)
+    {
+        printf(" %d \n",arr->ptr[i]);
+       return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void freeMemory(struct Queue *arr)
+{
+    free(arr->ptr);
+    arr->ptr = NULL;
+    arr = NULL;
+}
+int main(int argc, char const *argv[])
+{
+    struct Queue *que_obj = createQueue(que_obj,6);
+    printf("*** Enqueue ***\n");
+   
+    while (!isFull(que_obj))
+    {
+        que_obj = Enqueue(que_obj);
+    }
+    
+    
+   
+    printf("*** Peak ***\n");
+    while (peak(que_obj))
+    {
+        /* code */
+    }
+    printf("*** Dequeue ***\n ");
+    // while (!isEmpty(que_obj))
+    // {
+    //     Dequeue(que_obj);
+    // }
+    
+    freeMemory(que_obj);
+    return 0;
+}
