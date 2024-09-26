17943d9c84825473aae523d31457c7dd169b1b5f
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-09-26 12:22:53 +0500

    updated file

---
+++ linkListQueue.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node *front = NULL; 
+struct Node *rear = NULL;
+//This program is about implementation of Queue using Linklist
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+};
+struct Node *createNode(struct Node *head)
+{
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = 0;
+    head->next = NULL;
+}
+int isFull()
+{
+    struct Node *n = (struct Node * )malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }  
+} 
+int isEmpty(struct Node *front, struct Node *rear)
+{
+    if (front == NULL && rear == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    } 
+}
+void queueTraversal(struct Node *head)
+{
+    while (head != NULL)
+    {
+        printf(" %d-->",head->data);
+        head = head->next;
+
+    }  
+
+}
+struct Node *Enqueue(struct Node *head)
+{
+    int val;
+    struct Node *n = (struct Node * )malloc(sizeof(struct Node));
+    if (!isFull())
+    {
+        printf("Enter value of node: ");
+        scanf("%d",&val);
+        if (isEmpty(front, rear))
+        {
+            n->data = val;
+            front = n;
+            rear = n;
+            head = n;
+        }
+        else
+        {
+            n->data = val;
+            rear->next = n;
+            rear = n;
+        } 
+          return head;
+    }
+    
+}
+int Dequeue(struct Node *head)
+{
+
+}
+int main(int argc, char const *argv[])
+{
+   
+    struct Node *head = createNode(head);
+    int counter = 0;
+    printf("*** Enqueue *** \n");
+    while (counter < 5)
+    {
+        head = Enqueue(head);
+        counter++;
+    }
+    queueTraversal(head);
+
+    printf("\n *** Dequeue *** \n");
+    return 0;
+}
